e20960b58957facacf58e91ee027cacaf7b17414
Volodymyr <52545758+vladimirbag@users.noreply.github.com>
2023-06-16 00:37:03 +0300

    Add files via upload

---
+++ SecondClass.java
@@ -0,0 +1,11 @@
+public class SecondClass {
+    public int SecondValue = 27;
+
+    public SecondClass() {
+    }
+
+    public int getSecondValue() {
+        System.out.println("---" + this.SecondValue + "---");
+        return this.SecondValue;
+    }
+}
